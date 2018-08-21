217980d1afd17eab9605dbd333e1a59d6eb53e0e
Aric <aessig32@hotmail.com>
2018-08-21 15:33:25 -0700

    Caught errors. App finished. Optional refactor

--- App.js
+++ App.js
@@ -15,33 +15,35 @@ class App extends Component {
 		data: undefined,
 		data5Day: undefined,
 		tableIndex: undefined,
-		error: false
+		error: ""
 	}
 	getWeather = async (e) => {
+		this.resetError();
 		const city = e.target.elements.city.value;
 		const country = e.target.elements.country.value;
 		e.preventDefault();
-		const call_api = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=imperial&APPID=${api_key}`);
-		const data = await call_api.json();
-	
-		if (city && country) {
-			console.log(data);
+		try {
+			const call_api = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=imperial&APPID=${api_key}`);
+			const data = await call_api.json();
+			if (data) {
+				console.log(data);
+				this.setState({
+					data: data,
+					data5Day: [
+						data.list[0],
+						data.list[8],
+						data.list[16],
+						data.list[24],
+						data.list[32],
+					]
+				}) 
+		}
+		} catch(err) {
 			this.setState({
-				data: data,
-				data5Day: [
-					data.list[0],
-					data.list[8],
-					data.list[16],
-					data.list[24],
-					data.list[32],
-				]
-			}) 
-			console.log(this.state.data5Day);
-		} else {
-			console.log("error");
-			this.setState({
-				data: undefined
+				error: true
 			})
+			console.log("this is an error")
+			console.log(err);;
 		}
 	};
 
@@ -51,27 +53,42 @@ class App extends Component {
 		})
 		console.log(index);
 	}
-
+	resetError = () => {
+		this.setState({
+			error: false
+		})
+	}
   render() {
 	   const isLoading = typeof this.state.data === 'undefined';
-    return (
-      <div>
-        <Title/>
-        <Form getWeather={this.getWeather}/>
-         { isLoading
-           ? <h1 className="has-text-danger">Enter a city and state or city and country.</h1>
-           : [
-			        <p className="cityName has-text-danger">Daily weather for: {this.state.data.city.name}</p>,
-			       	<DayContainer data5Day={this.state.data5Day}
-			       								list={this.state.data.list}
-			       								changeTableAt={this.changeTableAt} />,
-			        <TableContainer list={this.state.data.list}
-		        									tableIndex={this.state.tableIndex}/>
-            ]
-         }
-      </div>
-    );
-  }
+	   if (this.state.error) {
+	   	return (
+		        <div>
+		        	<Title/>
+		        	<Form getWeather={this.getWeather}/>
+			   			<h1 className="has-text-danger">City and state not found</h1>
+			   		</div>
+			 )
+	   } else {
+		    return (
+		      <div>
+		        <Title/>
+		        <Form getWeather={this.getWeather}/>
+		         { isLoading
+		           ? <h1 className="has-text-danger">Enter a city and state or city and country.</h1>
+		           : [
+					        <p className="cityName has-text-danger">Daily weather for: {this.state.data.city.name}</p>,
+					       	<DayContainer data5Day={this.state.data5Day}
+					       								list={this.state.data.list}
+					       								changeTableAt={this.changeTableAt} />,
+					        <TableContainer list={this.state.data.list}
+				        									tableIndex={this.state.tableIndex}/>
+		            ]
+		         }
+		        
+		      </div>
+		    );
+		  }
+		}
 }
 
 export default App;
